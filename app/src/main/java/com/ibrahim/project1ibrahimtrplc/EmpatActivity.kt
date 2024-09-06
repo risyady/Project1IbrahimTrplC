29bdd6574045f1aa8e033a294f0abf02e23d5341
risyady <ibrahimrisyad28@gmail.com>
2024-09-06 22:49:32 +0700

    Add Intent
    - Menambah Intent dari TigaActivity ke EmpatActivity
    - Ganti warna jadi biru
    - Ganti warna Text pada xml lain
    - Ganti warna text di spinner_style
    - Menambah Border_style
    - Menambah Images
    - AndroidManifest dari mulai NilaiActivity jadi TigaActivity

--- app/src/main/java/com/ibrahim/project1ibrahimtrplc/EmpatActivity.kt
+++ app/src/main/java/com/ibrahim/project1ibrahimtrplc/EmpatActivity.kt
@@ -2,10 +2,22 @@ package com.ibrahim.project1ibrahimtrplc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class EmpatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empat)
+
+        val result = findViewById<TextView>(R.id.txtResult)
+        val bundle = intent.extras
+
+        if(bundle != null) {
+
+            result.text = "Transaction" +
+                    "\nProduct Name:\t ${bundle.getString("name")}" +
+                    "\nPrice\t\t: ${bundle.getString("price")} ${bundle.getString("piece")}" +
+                    "\nTotal\t\t: ${bundle.getString("total")}"
+        }
     }
 }
--- app/src/main/java/com/ibrahim/project1ibrahimtrplc/TigaActivity.kt
+++ app/src/main/java/com/ibrahim/project1ibrahimtrplc/TigaActivity.kt
@@ -3,8 +3,13 @@ package com.ibrahim.project1ibrahimtrplc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.*
 
-class TigaActivity : AppCompatActivity() {
+class TigaActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    val selectPiece = arrayOf<String>("  Meter", "  Gram", "  Piece(s)")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tiga)
@@ -15,6 +20,38 @@ class TigaActivity : AppCompatActivity() {
          *  implisit (kamera, browser) -> udah ada dalam android
          */
 
-        val intent = Intent(this@TigaActivity, EmpatActivity::class.java)
+        val product = findViewById<EditText>(R.id.editProduct)
+        val price = findViewById<EditText>(R.id.editPrice)
+        val piece = findViewById<Spinner>(R.id.spinPieces)
+        val total = findViewById<EditText>(R.id.editTotal)
+        val trans = findViewById<Button>(R.id.btnTransaction)
+
+        piece.onItemSelectedListener=this
+
+        val SpinPiece:ArrayAdapter<*> = ArrayAdapter<Any?>(this@TigaActivity, R.layout.spinner_style, selectPiece)
+        SpinPiece.setDropDownViewResource(R.layout.spinner_style)
+        piece.adapter = SpinPiece
+
+        trans.setOnClickListener {
+            val priceProduct = price.text.toString().toInt()
+            val totalProduct = total.text.toString().toInt()
+            val totalPrice = priceProduct * totalProduct
+            val intent = Intent(this@TigaActivity, EmpatActivity::class.java)
+            val bundle = Bundle()
+            bundle.putString("name", product.text.toString())
+            bundle.putString("price", price.text.toString())
+            bundle.putString("total", totalPrice.toString())
+            bundle.putString("piece", piece.selectedItem.toString())
+            intent.putExtras(bundle)
+            startActivity(intent)
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
     }
 }
